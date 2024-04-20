a3a69b90e4ab2117c660f178a9879571d393ac49
Gjorgji <gjorgjinaumoski@hotmail.com>
2024-04-20 11:12:55 +0200

    HOTFIX: Fixed footer styling and added minicssextractplugin to optimize the css loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -11,6 +12,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/index.html",
         }),
+        new MiniCssExtractPlugin(),
     ],
     devServer: {
         watchFiles: ["src/*.html"],
@@ -29,8 +31,8 @@ module.exports = {
                 },
             },
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
         ],
     },
