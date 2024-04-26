eeb02adc18e9eb369c37e3b3e60adf65f7c3ef6c
Gjorgji <gjorgjinaumoski@hotmail.com>
2024-04-26 21:19:00 +0200

    FIX: Changed showMessage function arguments to be an object

--- src/components/utility/message.js
+++ src/components/utility/message.js
@@ -1,7 +1,7 @@
 import "../../css/utility/message.css";
 
 // Expecting type to be one of the following values: SUCCES, INFO, DANGER, WARNING
-export const showMessage = (message, type) => {
+export const showMessage = ({message, type}) => {
     $(".movies-page").append(`<p class="message message--${type}">${message}</p>`);
     setTimeout(removeMessage, 3000);
 };
